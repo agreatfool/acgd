27b10ba32f2ad9148a5aac71ed1986c1ec763172
Jonathan Dai <nightghost5078@gmail.com>
2015-03-25 20:24:10 +0800

    Entrance done.

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-var ProgressBar = require('progress');
+var Logger = require('./lib/util/logger');
 
+var config = require('./config/config.json');
+var options = require('./options.json');
+
+Logger.info('System start ...');
+Logger.info('config: ' + JSON.stringify(config));
+Logger.info('options: ' + JSON.stringify(options));
+
+Logger.info('Load implementation class: ' + options.type);
+
+var typeStr = options.type.toLowerCase();
+var Implementation = require('./lib/' + typeStr + '/' + typeStr);
+
+var impl = new Implementation(options);
+
+impl.init(); // start to run
