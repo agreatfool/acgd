d2563897245ea9b9d5b429944730d13fcd76904f
Jonathan Dai <nightghost5078@gmail.com>
2015-04-23 12:51:33 +0800

    Save.

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 var Logger = require('./lib/util/logger');
 
 var config = require('./config/config.json');
-var options = require('./options.json');
+var options = require('./options.js');
 
 Logger.info('System start ...');
 Logger.info('config: ' + JSON.stringify(config));
-Logger.info('options: ' + JSON.stringify(options));
 
-Logger.info('Load implementation class: ' + options.type);
+var type = options.TYPE;
+var typeOptions = options[options.TYPE];
+Logger.info('options: ' + JSON.stringify(typeOptions));
 
-var typeStr = options.type.toLowerCase();
-var Implementation = require('./lib/' + typeStr + '/' + typeStr);
+Logger.info('Load implementation class: ' + options.TYPE);
 
-var impl = new Implementation(options);
+var Implementation = require('./lib/' + type.toLowerCase() + '/' + type.toLowerCase());
+
+var impl = new Implementation(typeOptions);
 
 impl.init(); // start to run
+
+//==========
+
+var fs = require("fs");
+var async = require('async');
+var cheerio = require('cheerio');
+var needle = require('needle');
+var liburl = require('url');
+
+var url = 'http://www.xindm.cn/mh/HOLIDAY-LOVE/';
+
+var urlparts = liburl.parse(url);
+
+var host = urlparts.protocol + '//' + urlparts.host;
+
+needle.get(url, function(error, response) {
+    if (!error && response.statusCode == 200) {
+        var $ = cheerio.load(response.body);
+
+        $('#mhlist ul li a').each(function() {
+            var match = $(this).attr('onclick').toString().match(/^window.open\('(.+)'\)$/);
+            if (match !== null) {
+                console.log($(this).attr('title') + ' : ' + host + match[1]);
+            }
+        });
+    }
+});
+
+
+
--- options.js
+++ options.js
@@ -1,16 +1,45 @@
-{
-  "type": "MANGA",
+var options = {
+  "TYPE": "MANGA",
   "MANGA": {
     "AlubmUrl": "http://www.manhua8.net/manhua/4250/",
-    "AlbumNameSelector": ".bookInfo h1 b",
-    "AlbumUlSelector": ".bookList ul",
-    "AlbumLiSelector": ".new",
-    "MangaImgSelector": "#comicImg",
-    "MangaTotalPageSelector": "#topSelect",
-    "MangaNextPagePattern": "/(\.htm)$|(\.html)$/",
-    "MangaNextPageReplace": "function() { return arguments[0] + '?p=1'; }"
+    "Sites": {
+      "www.xindm.cn": {
+          // TODO AlbumLiLinkSelector 还是要优化，有的是a标签里的onclick事件上做跳转
+          "AlbumNameSelector": ".fengmian_comic_head_con_r h1",
+          "AlbumListSelector": "#mhlist ul",
+          "AlbumLiLinkSelector": "a.new",
+          // TODO 是不是需要添加一个Li的标题攫取？
+          "MangaImgSelector": "#comicImg",
+          "MangaTotalPageSelector": "#topSelect",
+          // http://www.manhua8.net/manhua/4250/list_70203.htm => http://www.manhua8.net/manhua/4250/list_70203.htm?p=3
+          "MangaNextPagePattern": /(\.htm)$/,
+          "MangaNextPageReplace": function(pageNo) { return arguments[0] + '?p=' + pageNo; }
+      },
+      "www.manhua8.net": {
+          "AlbumNameSelector": ".bookInfo h1 b",
+          "AlbumListSelector": ".bookList ul",
+          "AlbumLiLinkSelector": "a.new",
+          "MangaImgSelector": "#comicImg",
+          "MangaTotalPageSelector": "#topSelect",
+          // http://www.manhua8.net/manhua/4250/list_70203.htm => http://www.manhua8.net/manhua/4250/list_70203.htm?p=3
+          "MangaNextPagePattern": /(\.htm)$/,
+          "MangaNextPageReplace": function(pageNo) { return arguments[0] + '?p=' + pageNo; }
+      },
+      "www.seemh.com": {
+          "AlbumNameSelector": ".book-title h1",
+          "AlbumListSelector": ".chapter-list-0 ul",
+          "AlbumLiLinkSelector": "a",
+          "MangaImgSelector": "#mangaFile",
+          "MangaTotalPageSelector": "#pageSelect",
+          // http://www.seemh.com/comic/6664/57813.html => http://www.seemh.com/comic/6664/57813_p2.html
+          "MangaNextPagePattern": /(\.html)$/,
+          "MangaNextPageReplace": function(pageNo) { return '_p' + pageNo + arguments[0]; }
+      }
+    }
   },
   "VIDEO": {},
   "LIST": {},
   "STRING": {}
-}
+};
+
+module.exports = options;
