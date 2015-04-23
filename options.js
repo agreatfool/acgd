59af7d88fc24061abb3e11600766ea3cf074a9ea
Jonathan Dai <nightghost5078@gmail.com>
2015-04-23 12:51:13 +0800

    Fix name & move position.

---
+++ options.js
@@ -0,0 +1,16 @@
+{
+  "type": "MANGA",
+  "MANGA": {
+    "AlubmUrl": "http://www.manhua8.net/manhua/4250/",
+    "AlbumNameSelector": ".bookInfo h1 b",
+    "AlbumUlSelector": ".bookList ul",
+    "AlbumLiSelector": ".new",
+    "MangaImgSelector": "#comicImg",
+    "MangaTotalPageSelector": "#topSelect",
+    "MangaNextPagePattern": "/(\.htm)$|(\.html)$/",
+    "MangaNextPageReplace": "function() { return arguments[0] + '?p=1'; }"
+  },
+  "VIDEO": {},
+  "LIST": {},
+  "STRING": {}
+}
